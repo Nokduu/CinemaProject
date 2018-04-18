6b672fb991c0b56ea08a508918dbbb96c5480755
emt00 <emt00@EMT09>
2018-04-18 16:29:20 +0900

    180418429

--- Project/Forms/LoginForm.Designer.cs
+++ Project/Forms/LoginForm.Designer.cs
@@ -2,6 +2,7 @@
 {
     partial class LoginForm
     {
+        
         /// <summary>
         /// 필수 디자이너 변수입니다.
         /// </summary>
@@ -28,57 +29,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.TB_id = new System.Windows.Forms.TextBox();
             this.BT_login = new System.Windows.Forms.Button();
             this.TB_pw = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TB_id
             // 
-            this.TB_id.Location = new System.Drawing.Point(316, 179);
+            this.TB_id.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TB_id.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TB_id.Location = new System.Drawing.Point(444, 402);
             this.TB_id.Name = "TB_id";
-            this.TB_id.Size = new System.Drawing.Size(100, 21);
+            this.TB_id.Size = new System.Drawing.Size(275, 32);
             this.TB_id.TabIndex = 0;
             // 
             // BT_login
             // 
-            this.BT_login.Location = new System.Drawing.Point(422, 179);
+            this.BT_login.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BT_login.BackgroundImage")));
+            this.BT_login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BT_login.FlatAppearance.BorderSize = 0;
+            this.BT_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BT_login.Location = new System.Drawing.Point(278, 609);
             this.BT_login.Name = "BT_login";
-            this.BT_login.Size = new System.Drawing.Size(75, 48);
+            this.BT_login.Size = new System.Drawing.Size(221, 71);
             this.BT_login.TabIndex = 2;
-            this.BT_login.Text = "button1";
             this.BT_login.UseVisualStyleBackColor = true;
             this.BT_login.Click += new System.EventHandler(this.BT_login_Click);
+            this.BT_login.Paint += new System.Windows.Forms.PaintEventHandler(this.BT_login_Paint);
             // 
             // TB_pw
             // 
-            this.TB_pw.Location = new System.Drawing.Point(316, 206);
+            this.TB_pw.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TB_pw.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TB_pw.Location = new System.Drawing.Point(444, 474);
             this.TB_pw.Name = "TB_pw";
-            this.TB_pw.Size = new System.Drawing.Size(100, 21);
+            this.TB_pw.Size = new System.Drawing.Size(275, 32);
             this.TB_pw.TabIndex = 1;
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(316, 234);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "회원가입";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(565, 609);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 71);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(1001, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 36);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(951, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 36);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(13, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1037, 36);
+            this.panel1.TabIndex = 6;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1060, 795);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.TB_pw);
             this.Controls.Add(this.BT_login);
             this.Controls.Add(this.TB_id);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginForm";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.LoginForm_Paint);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,7 +152,10 @@
         private System.Windows.Forms.TextBox TB_id;
         private System.Windows.Forms.Button BT_login;
         private System.Windows.Forms.TextBox TB_pw;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Project/Forms/SignUp.cs
+++ Project/Forms/SignUp.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Project.Database.Model;
 using Project.Database;
+using System.Drawing.Drawing2D;
 
 namespace Project.Forms
 {
@@ -35,6 +36,23 @@ namespace Project.Forms
 
             DBUSE dbuse = new DBUSE();
             dbuse.SignUp(memberTbl);
+        }
+
+        private void SignUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Rectangle Rect = new Rectangle(0, 0, this.Width, this.Height);
+            GraphicsPath GraphPath = new GraphicsPath();
+            GraphPath.AddArc(Rect.X, Rect.Y, 50, 50, 180, 90);
+            GraphPath.AddArc(Rect.X + Rect.Width - 50, Rect.Y, 50, 50, 270, 90);
+            GraphPath.AddArc(Rect.X + Rect.Width - 50, Rect.Y + Rect.Height - 50, 50, 50, 0, 90);
+            GraphPath.AddArc(Rect.X, Rect.Y + Rect.Height - 50, 50, 50, 90, 90);
+            this.Region = new Region(GraphPath);
         }
     }
 }
